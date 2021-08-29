c1aee727fc94516699b5aa2721692771736c7646
unknown <slamet.supriyadi@bidbox.id>
2021-08-29 16:23:59 +0700

    fixing unit testing

--- test/movies_test.go
+++ test/movies_test.go
@@ -11,38 +11,29 @@ import (
 
 func TestSearchMovie(t *testing.T) {
 	var conn *grpc.ClientConn
-	// conn, err := grpc.Dial("13.228.12.30:30304", grpc.WithInsecure()) // for staging server testing
 	conn, err := grpc.Dial(":8090", grpc.WithInsecure())
 	assert.Equal(t, nil, err)
 	if err != nil {
 		t.Fatalf("did not connect: %s", err)
 	}
 	defer conn.Close()
-
 	c := proto.NewMoviesClient(conn)
 	response, err := c.Search(context.Background(), &proto.SearchRequestMessage{Search: "batman", Page: 1})
-	// if err!=nil{
-	// 	t.Fatalf("error %s",err)
-	// }
 	assert.Equal(t, nil, err)
 	t.Log(response)
+	
 }
 
 func TestDetailMovie(t *testing.T) {
 	var conn *grpc.ClientConn
-	// conn, err := grpc.Dial("13.228.12.30:30304", grpc.WithInsecure()) // for staging server testing
 	conn, err := grpc.Dial(":8090", grpc.WithInsecure())
 	assert.Equal(t, nil, err)
 	if err != nil {
 		t.Fatalf("did not connect: %s", err)
 	}
 	defer conn.Close()
-
 	c := proto.NewMoviesClient(conn)
 	response, err := c.GetDetail(context.Background(), &proto.DetailMovieRequestMessage{Id: "tt0096895"})
-	// if err!=nil{
-	// 	t.Fatalf("error %s",err)
-	// }
 	assert.Equal(t, nil, err)
 	t.Log(response)
 }
